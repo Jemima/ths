54e6fe72b60d1a7640de9bf5e3c75a9df8622e2a
David Munro <dsmu186@kraken.cse.unsw.EDU.AU>
2013-05-22 16:32:53 +1000

    Changes to run scripts

--- workflow/run.sh
+++ workflow/run.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 for n in {1..10}
 do
    mkdir out/$n
    echo "Run $n"
-   for i in 1 2e-3 1e-3 9e-4 8e-4 5e-4 1e-4
+   for i in 1 2e-3 1e-3 9e-4 8.88e-4 8e-4 5e-4 2.5e-4 1e-4 5e-5 1e-5
    do
       echo "Target Recovery Time $i"
-      ./generate.py -t -r $i -l out/$n/$i.log -R out/$n/$i.results blifs /tmp
+      ./generate.py -n 8 -t -r $i -l out/$n/$i.log -R out/$n/$i.results blifs /tmp
    done
 done
