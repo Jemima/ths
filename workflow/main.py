a92dbeeebde5551db4eac95a7038c900b7f32890
David Munro <dweia666@yahoo.com>
2013-06-03 17:59:42 +1000

    More results. Updated main.py to better time things

--- workflow/main.py
+++ workflow/main.py
@@ -10,7 +10,7 @@ import time
 from multiprocessing import Pool
 
 if __name__ == "__main__":
-   start = time.clock()
+   start = time.time()
    parser = argparse.ArgumentParser(description="Partition and TMR a circuit")
    parser.add_argument("infile", type=str, help="Input blif format circuit")
    parser.add_argument("outfile", type=str, help="Output blif format circuit")
@@ -20,6 +20,7 @@ if __name__ == "__main__":
    parser.add_argument("-b", "--breadthfirst", action="store_true", help="Partitioner should use worse breadth first traversal")
    parser.add_argument("-r", "--recoverytime", type=str, help="Maximum per partition recovery time")
    parser.add_argument("-n", "--numthreads", type=int, help="Maximum number of threads to use")
+   parser.add_argument("-T", "--timing", action="store_true", help="Suppresses all output except timing information")
 
    params = parser.parse_args()
    dir = tempfile.mkdtemp(dir=os.getcwd())+"/"
@@ -32,8 +33,9 @@ if __name__ == "__main__":
       dirSplit = dir+"Split/"
       os.mkdir(dirTMR)
       os.mkdir(dirSplit)
-      sys.stderr.write("Partitioning...\n")
-      sys.stderr.write(params.outfile)
+      if params.timing != True:
+         sys.stderr.write("Partitioning...\n")
+         sys.stderr.write(params.outfile)
       critical = params.latency
       if critical == None:
          critical = 0.00000001
@@ -51,11 +53,13 @@ if __name__ == "__main__":
          raise
       inputs = output[0] #The original inputs and outputs. We save these, since in the process of splitting loops, creating partitions, etc, we create a lot of extra inputs and outputs,
       outputs = output[1] #and we aren't able to tell which are supposed to be present in the end result otherwise
-      print(params.infile+"\t"+output[2],end='\t')
+      if params.timing != True:
+         print(params.infile+"\t"+output[2],end='\t')
       partitions = output[3:]
-      step1 = time.clock()
+      step1 = time.time()
       
-      sys.stderr.write("Triplicating...\n")
+      if params.timing != True:
+         sys.stderr.write("Triplicating...\n")
       threads = params.numthreads
       if threads == None:
          pool = Pool(4)
@@ -63,8 +67,9 @@ if __name__ == "__main__":
          pool = Pool(threads);
       TMRArgs = [["python3", "blifTMR.py", "voter.blif", dirSplit+file, dirTMR+file] for file in os.listdir(dirSplit)]
       pool.map(subprocess.check_call, TMRArgs, 4)
-      step2 = time.clock()
-      sys.stderr.write("Merging...\n")
+      step2 = time.time()
+      if params.timing != True:
+         sys.stderr.write("Merging...\n")
       header = open(dir+"header.blif", 'w')
       header.write(".model main\n.inputs ")
       header.write(inputs)
@@ -73,8 +78,9 @@ if __name__ == "__main__":
       header.close()
       subprocess.check_call(["python3", "blifJoin.py", dir+"file.blif", dir+"header.blif", "-f", dirTMR+"*.blif"])
       
-      step3 = time.clock()
-      sys.stderr.write("Flattening...\n");
+      step3 = time.time()
+      if params.timing != True:
+         sys.stderr.write("Flattening...\n");
       subprocess.check_output(["./abc", "-o", params.outfile, "-c", "echo", dir+"file.blif"])
       #ABC has a few assorted bugs. 1. It strips clock information from latches, resulting in invalid blif files. Assume there is only one clock for all latches (true for MCNC) and sed to fix it up
       try:
@@ -82,6 +88,7 @@ if __name__ == "__main__":
          subprocess.check_call(["sed", "-ri", "s/(\\.latch.+)(2)/\\1 "+latch[3]+" "+latch[4]+" 2/", params.outfile])
       except subprocess.CalledProcessError: #Ignore these errors. Grep returns 1 when no match found, which causes an exception
          pass
+      step4 = time.time()
       if params.test:
          if params.count and params.count < inputs.count(' '):
             sys.stderr.write("Skipping test due to too many inputs\n")
@@ -90,9 +97,11 @@ if __name__ == "__main__":
             f = open(params.outfile)
             s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
             if s.find(bytes('.latch', 'UTF-8')) != -1:
-               sys.stderr.write("Latches present.\n")
+               if params.timing != True:
+                  sys.stderr.write("Latches present.\n")
                output = str(subprocess.check_output(["./abc", "-c", "dsec "+params.infile+" "+params.outfile]), "UTF-8")
-               sys.stderr.write(output)
+               if params.timing != True:
+                  sys.stderr.write(output)
                if not "are equivalent" in output:
                   exit(27)
                s.close()
@@ -100,21 +109,24 @@ if __name__ == "__main__":
                s.close()
                sys.stderr.write("Testing...\n")
                output = str(subprocess.check_output(["./abc", "-c", "cec "+params.infile+" "+params.outfile]), "UTF-8")
-               sys.stderr.write(output)
+               if params.timing != True:
+                  sys.stderr.write(output)
                if not "are equivalent" in output:
                   exit(5)
                #subprocess.check_call(["python3", "test.py", params.infile, params.outfile])
                   exit(27)
                #subprocess.check_call(["python", "test.py", params.infile, params.outfile])
-      step4 = time.clock()
+      step5 = time.time()
       print(step1-start, end='\t')
       print(step2-step1, end='\t')
       print(step3-step2, end='\t')
       print(step4-step3, end='\t')
-      print(time.clock()-start, end='\t')
+      print(step5-step4, end='\t')
+      print(time.time()-start, end='\t')
       print(len(TMRArgs), end='\n')
-      for s in partitions:
-         print(s)
+      if params.timing != True:
+         for s in partitions:
+            print(s)
       shutil.rmtree(dir)
    except subprocess.CalledProcessError as e:
       sys.stderr.write("Subprocess error: "+str(e.output))
