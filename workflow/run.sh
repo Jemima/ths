31392ab64a257da02984311973c486b01faaa814
David Munro <dsmu186@kraken.cse.unsw.EDU.AU>
2013-06-03 17:54:11 +1000

    More results

--- workflow/run.sh
+++ workflow/run.sh
@@ -3,13 +3,13 @@
 Folder=outBFS
 echo $Folder
 mkdir /tmp/dsmu186blifs/$Folder
-for n in {6..15}
+for n in {1..15}
 do
    mkdir $Folder/$n
    echo "Run $n"
-   for i in 1e-3 2.5e-4 7.5e-5
+   for i in 1.2e-4
    do
       echo "Target Recovery Time $i"
-      ./generate.py -n 2 -t -r $i -b -l $Folder/$n/$i.log -R $Folder/$n/$i.results blifs /tmp/dsmu186blifs/$Folder/
+      ./generate.py -n 4 -t -r $i -b -l $Folder/$n/$i.log -R $Folder/$n/$i.results blifs /tmp/dsmu186blifs/$Folder/
    done
 done
